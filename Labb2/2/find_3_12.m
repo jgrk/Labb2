02efcab90099e0dbb0c9a529ee9c9b3781bf2534
Erik Duvander <e.o.duvander@gmail.com>
2022-11-08 14:43:37 +0100

    mer gjort

--- Labb2/2/find_3_12.m
+++ Labb2/2/find_3_12.m
@@ -3,7 +3,7 @@ a=0; b=3.6; n=1;
 while true
 
     x=linspace(a,b,n);
-    if isempty(find(x==3.12)) == 1
+    if isempty(find(x==3.12,1)) == 1
         n=n+1;
     else
         break
--- Labb2/3/main.m
+++ Labb2/3/main.m
@@ -1,2 +1,8 @@
 %%%Inskjutningsmetoden
 
+
+k=@(x)(2+x/3); dk=1/3; %Givna funktionen k
+Q=@(x)(280*exp(-(x-1.8)^2)); %Givna funktionen Q
+
+
+
